d4dcf51cd8417e8b5f47fa2108acb522c8368bdc
Vijay Pothamsetty <Vijay.Pothamsetty@copart.com>
2018-08-06 12:01:49 +0530

    [3][6][Understanding JSX]

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>I'm React App First Time </h1>
-      </div>
-    );
+    // return (
+    //   <div className="App">
+    //     <h1>I'm React App First Time </h1>
+    //   </div>
+    // );
+    return React.createElement('div', {className : 'App'}, React.createElement('h1', null, 'Is it working?'));
   }
 }
 
