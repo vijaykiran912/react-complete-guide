9c9c1a39bf4f753493a4166695014b8fa89023f2
Vijay Pothamsetty <Vijay.Pothamsetty@copart.com>
2018-08-07 10:33:32 +0530

    [3][8][Creating a Functional Component]

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>I'm React App First Time </h1>
-    //   </div>
-    // );
-    return React.createElement('div', {className : 'App'}, React.createElement('h1', null, 'Is it working?'));
+    return (
+      <div className="App">
+        <h1>I'm React App </h1>
+        <p> This is working </p>
+        <Person />
+      </div>
+    );
   }
 }
 
---
+++ src/Person/Person.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const person = () => {
+    return(
+        <p>This is Person </p>
+    )
+}
+
+export default person;
