e7f9faf59b7aad8b8ed59fd1beed4efbec2ddd83
vipothamse-copart <Vijay.Pothamsetty@copart.com>
2018-08-12 14:41:19 +0530

    [3][13][Understanding the Children Property]

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ class App extends Component {
       <div className="App">
         <h1>I'm React App </h1>
         <p> This is working </p>
-        <Person name="Vijay" age="29"/>
-        <Person name="Kohli" age="30"/>
-        <Person name="Pujara" age="32"/>
+        <Person name="Vijay" age="29" />
+        <Person name="Kohli" age="30"> My hobbies: Coding </Person>
+        <Person name="Pujara" age="32" />
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
 const person = (props) => {
-    return(
-        <p>I am {props.name}, {props.age} year's of age</p>
+    return (
+        <div>
+            <p>I am {props.name}, {props.age} year's of age</p>
+            <p>{props.children}</p>
+        </div>
     )
 }
 
