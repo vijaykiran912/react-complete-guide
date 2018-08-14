7ebd5fc26957e6a872e5c14ee4474c475a5ed965
vipothamse-copart <Vijay.Pothamsetty@copart.com>
2018-08-14 11:12:35 +0530

    [4][3][Handling Dynamic Content The JavaScript Way]

--- src/App.js
+++ src/App.js
@@ -53,6 +53,26 @@ class App extends Component {
       cursor: 'pointer'
     }
 
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (<div>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age} />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          nameChanged={this.nameChangedHandler}>
+          My hobbies: Coding
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+          clicked={() => this.swithButtonHandler('Vijay Reddy')}>
+        </Person>
+      </div>)
+    }
+
     return (
       <div className="App">
         <h1>I'm React App </h1>
@@ -60,24 +80,7 @@ class App extends Component {
         <button style={style} onClick={this.tooglePersonsHandler}>
           Switch Button
         </button>
-        {this.state.showPersons ?
-          <div>
-            <Person
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age} />
-            <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-              nameChanged={this.nameChangedHandler}>
-              My hobbies: Coding
-            </Person>
-            <Person
-              name={this.state.persons[2].name}
-              age={this.state.persons[2].age}
-              clicked={() => this.swithButtonHandler('Vijay Reddy')}>
-            </Person> 
-          </div> : null
-        }
+        {persons}
       </div>
     );
   }
